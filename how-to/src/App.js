cfe4a7d2b8c8f0bed2c8820e1609f4e975da7294
matt22881 <matt22881@gmail.com>
2020-11-17 01:04:52 -0500

    change some file names to be less offensive

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
-import XSignup from './XSignup'
-import XSignIn from './XSignIn'
+import Signup from './Signup'
+import SignIn from './SignIn'
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/signin' component={XSignIn} />
-        <Route path= '/signup' component={XSignup} />      
+        <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={Signup} />      
       </Switch>
     </div>
   );
